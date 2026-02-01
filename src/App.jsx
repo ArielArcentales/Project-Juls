004936908f2afce7aed836b585be06cec53e6f0b
Ariel Arcentales <amarcentales@puce.edu.ec>
2026-01-31 22:23:00 -0500

    Primer Commit J

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div style={{ width: '100%', height: '100vh', background: '#000', color: '#fff' }}>
+      <h1>Aquí comenzará el jardín para ella...</h1>
+    </div>
+  )
+}
+
+export default App
